6eb5b35b934e150a308c97c31f96d8940a51ec67
Nicoxdzin <nimarques1207@gmail.com>
2024-06-20 23:39:40 -0300

    Updating project structure

---
+++ electron-studies/src/renderer/src/App.jsx
@@ -0,0 +1,15 @@
+import Versions from './components/Versions'
+
+function App() {
+  // const ipcHandle = () => window.electron.ipcRenderer.send('ping')
+
+  return (
+    <>
+      <h1>Hello World</h1>
+      {/* <Versions></Versions> */}
+    </>
+  )
+}
+
+export default App
+
